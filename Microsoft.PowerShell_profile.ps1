5312d024c6882fc23d9a0f515c37de51d87a5658
Riasat Sk <67420883+riasatsk@users.noreply.github.com>
2024-09-05 11:43:52 +0530

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,17 +1,3 @@
-function run {
-    param([string]$file)
-
-    $extension = [System.IO.Path]::GetExtension($file)
-
-    switch ($extension) {
-        ".py" { python $file; break }
-        ".js" { node $file; break }
-        ".ts" { bun run $file; break }
-        default { Write-Host "Unsupported file type. Only .py and .js files are supported." }
-    }
-}
-
-
 function download {
      python "C:\Scripts\download.py" 
 }
